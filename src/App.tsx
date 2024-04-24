bfd58a716f556f51f1efbd804b823eee74223724
이윤슬 <ysisys0202@gmail.com>
2024-04-24 13:47:33 +0900

    ♻️  path 상수 선언, 경로에 path 상수 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
+import path from "./constants/path";
 function App() {
   return (
     <>
       <div>
-        <Link to="/">HOME</Link>
-        <Link to="/posts">POST LIST</Link>
-        <Link to="/posts/:id">POST DETAIL</Link>
-        <Link to="/posts/new">NEW POST</Link>
-        <Link to="/posts/edit/:id">POST EDIT</Link>
-        <Link to="/profile">PROFILE</Link>
+        <Link to={path.home}>HOME</Link>
+        <Link to={path.postList}>POST LIST</Link>
+        <Link to={path.postDetail}>POST DETAIL</Link>
+        <Link to={path.postNew}>NEW POST</Link>
+        <Link to={path.postEdit}>POST EDIT</Link>
+        <Link to={path.profile}>PROFILE</Link>
       </div>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>}></Route>
-        <Route path="/posts" element={<h1>Post List Page</h1>}></Route>
-        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>}></Route>
-        <Route path="/posts/new" element={<h1>New Post Page</h1>}></Route>
-        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>}></Route>
-        <Route path="/profile" element={<h1>Profile Page</h1>}></Route>
+        <Route path={path.home} element={<h1>Home Page</h1>}></Route>
+        <Route path={path.postList} element={<h1>Post List Page</h1>}></Route>
+        <Route
+          path={path.postDetail}
+          element={<h1>Post Detail Page</h1>}
+        ></Route>
+        <Route path={path.postNew} element={<h1>New Post Page</h1>}></Route>
+        <Route path={path.postEdit} element={<h1>Post Edit Page</h1>}></Route>
+        <Route path={path.profile} element={<h1>Profile Page</h1>}></Route>
         <Route path="*" element={<Navigate replace to="/" />}></Route>
       </Routes>
     </>
---
+++ src/constants/path.ts
@@ -0,0 +1,10 @@
+const path = {
+  home: "/",
+  postList: "/posts",
+  postDetail: "/posts/:id",
+  postNew: "/posts/new",
+  postEdit: "/posts/edit/:id",
+  profile: "/profile",
+};
+
+export default path;
