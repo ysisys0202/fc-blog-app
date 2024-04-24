937707cf61ade18734c626e92706c3e708dfc3b4
이윤슬 <ysisys0202@gmail.com>
2024-04-24 16:01:39 +0900

    💄 기본 레이아웃 스타일 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import path from "constants/path";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to={path.postNew}>글쓰기</Link>
+          <Link to={path.postList}>게시글</Link>
+          <Link to={path.profile}>프로필</Link>
+        </div>
+      </header>
+      <div className="post-list">Post List</div>
+      <footer>
+        <div>menu 1</div>
+        <div>menu 2</div>
+        <div>menu 3</div>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
