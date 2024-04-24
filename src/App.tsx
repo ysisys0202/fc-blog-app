8a16a7b52ddfa5b11055b1ba352f5515a266c3ce
이윤슬 <ysisys0202@gmail.com>
2024-04-24 13:39:33 +0900

    ✨ react-router-dom 라이브러리 설치 및 route 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Link to="/">HOME</Link>
+        <Link to="/posts">POST LIST</Link>
+        <Link to="/posts/:id">POST DETAIL</Link>
+        <Link to="/posts/new">NEW POST</Link>
+        <Link to="/posts/edit/:id">POST EDIT</Link>
+        <Link to="/profile">PROFILE</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/posts" element={<h1>Post List Page</h1>}></Route>
+        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>}></Route>
+        <Route path="/posts/new" element={<h1>New Post Page</h1>}></Route>
+        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile Page</h1>}></Route>
+        <Route path="*" element={<Navigate replace to="/" />}></Route>
+      </Routes>
+    </>
   );
 }
 
