2fa445bf1d69a56baf2ec14a6ed8e4553e4aa85a
이윤슬 <ysisys0202@gmail.com>
2024-04-25 12:10:35 +0900

    💄 newPost Page 작업

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,7 +1,15 @@
+import Footer from "components/Footer";
+import Header from "components/Header";
 import React from "react";
 
 const NewPost = () => {
-  return <div>NewPost</div>;
+  return (
+    <>
+      <Header />
+      <div>post form</div>
+      <Footer />
+    </>
+  );
 };
 
 export default NewPost;
