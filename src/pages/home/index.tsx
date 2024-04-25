35f23c007f116032c4aa24296fea6ee23a32188b
이윤슬 <ysisys0202@gmail.com>
2024-04-25 12:40:15 +0900

    💄 Carousel 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,11 +2,13 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import PostNavigation from "components/PostNavigation";
 import PostList from "components/PostList";
+import Carousel from "components/Carousel";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Carousel />
       <PostNavigation />
       <PostList />
       <Footer />
