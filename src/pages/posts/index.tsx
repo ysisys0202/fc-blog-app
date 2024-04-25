09b54de47370928d18743d2b752f6e08a43bfe63
이윤슬 <ysisys0202@gmail.com>
2024-04-25 12:10:12 +0900

    💄  PostListPage 작업

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PostList from "components/PostList";
 
-const PostList = () => {
-  return <div>PostList</div>;
+const PostListPage = () => {
+  return (
+    <>
+      <Header />
+      <PostList />
+      <Footer />
+    </>
+  );
 };
 
-export default PostList;
+export default PostListPage;
