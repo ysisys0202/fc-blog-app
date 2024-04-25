60fce4d38141660fc721583650a7ca8c1d1f88ad
이윤슬 <ysisys0202@gmail.com>
2024-04-25 13:16:47 +0900

    💄 PostNavigation active 기능 추가

--- src/components/PostNavigation.tsx
+++ src/components/PostNavigation.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+type TabType = "all" | "my";
 
 const PostNavigation = () => {
+  const [activeTab, setActiveTab] = useState<TabType>("all");
+
+  function handleTabClick(tabName: TabType) {
+    setActiveTab(tabName);
+  }
+
   return (
     <nav className="post__navigation">
       <ul className="post__navigation-list">
-        <li className="post__navigation-item post__navigation-item--active">
-          전체
-        </li>
-        <li className="post__navigation-item ">나의 글</li>
+        {TAB_LIST.map((tab) => (
+          <li
+            key={tab.nameEn}
+            role="presentation"
+            className={`post__navigation-item ${
+              activeTab === tab.nameEn ? "post__navigation-item--active" : ""
+            }`}
+            onClick={handleTabClick.bind(null, tab.nameEn)}
+          >
+            {tab.nameKor}
+          </li>
+        ))}
       </ul>
     </nav>
   );
 };
+
+const TAB_LIST: { nameEn: TabType; nameKor: string }[] = [
+  {
+    nameEn: "all",
+    nameKor: "전체",
+  },
+  {
+    nameEn: "my",
+    nameKor: "나의 글",
+  },
+];
 
 export default PostNavigation;
