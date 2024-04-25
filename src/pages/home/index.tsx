1cfd42207222ae847679a9e5e95fdec6686ca525
이윤슬 <ysisys0202@gmail.com>
2024-04-25 11:05:58 +0900

    💄 Home 컴포넌트 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
-import path from "constants/path";
 import { Link } from "react-router-dom";
+import path from "constants/path";
 
 const Home = () => {
   return (
@@ -11,11 +11,48 @@ const Home = () => {
           <Link to={path.profile}>프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post List</div>
+      <nav className="post__navigation">
+        <ul className="post__navigation-list">
+          <li className="post__navigation-item post__navigation-item--active">
+            전체
+          </li>
+          <li className="post__navigation-item ">나의 글</li>
+        </ul>
+      </nav>
+      <div className="post__list">
+        {[...Array(10)].map((item, index) => (
+          <div key={index} className="post__box">
+            <Link to={`${path.postDetail}/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <span className="post__author-name">패스트캠퍼스</span>
+                <span className="post__date">2024.04.25</span>
+              </div>
+              <h3 className="post__title">게시글</h3>
+              <p className="post__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                sit consequatur omnis animi voluptates magnam ea ut, nobis
+                laudantium recusandae non deserunt hic vero illum neque
+                asperiores nam magni incidunt? Quam ipsum rem error labore
+                assumenda, modi impedit ducimus, inventore deleniti cum incidunt
+                nesciunt dicta sint aliquam facere. Ea, vel.
+              </p>
+              <div className="post_utils-box">
+                <button type="button" className="post__delete">
+                  삭제
+                </button>
+                <button type="button" className="post__edit">
+                  수정
+                </button>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>menu 1</div>
-        <div>menu 2</div>
-        <div>menu 3</div>
+        <Link to={path.postNew}>글쓰기</Link>
+        <Link to={path.postList}>게시글</Link>
+        <Link to={path.profile}>프로필</Link>
       </footer>
     </div>
   );
